4a93c3c1f7438c2d47f367dd3dfbb2febcd264bd
npdrums <nikola.pujaz@gmail.com>
2020-08-21 15:17:43 +0200

    Built basic API, Added Repository

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+        public ProductsController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _repository.GetProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _repository.GetProductByIdAsync(id);
+        }
+
+    }
+}
