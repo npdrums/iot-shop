9ca9681eac082930bb7866672a85fc68b1a1f411
npdrums <nikola.pujaz@gmail.com>
2020-09-01 00:38:02 +0200

    CamelCase Fix in Response 2

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -39,7 +39,7 @@ namespace API.Middleware
                     : new ApiException((int) HttpStatusCode.InternalServerError);
                 
                 var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
-                var json = JsonSerializer.Serialize(response);
+                var json = JsonSerializer.Serialize(response, options);
                 await context.Response.WriteAsync(json);
             }
         }
