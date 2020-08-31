96ad41c76f0512464a2a1234c21a63a0c590b8a7
npdrums <nikola.pujaz@gmail.com>
2020-08-31 22:31:48 +0200

    Fixed Picture URL

---
+++ API/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,25 @@
+using API.DTOs;
+using AutoMapper;
+using Core.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace API.Helpers
+{
+    public class ProductUrlResolver : IValueResolver<Product, ProductDTO, string>
+    {
+        private readonly IConfiguration _configuration;
+        public ProductUrlResolver(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string Resolve(Product source, ProductDTO destination, string destMember, ResolutionContext context)
+        {
+            if(!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return _configuration["ApiUrl"] + source.PictureUrl;
+            }
+            return null;
+        }
+    }
+}
