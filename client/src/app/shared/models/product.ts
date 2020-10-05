9b2d3f96951929fe01ebc418f12db6152dbcaa8b
npdrums <nikola.pujaz@gmail.com>
2020-10-05 15:56:34 +0200

    Add Models

---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    pictureUrl: string;
+    productBrand: string;
+    productType: string;
+}
