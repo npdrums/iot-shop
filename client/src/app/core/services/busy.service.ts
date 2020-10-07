48520cf52b33f47026d0a5aac07503fdd43c0f9a
npdrums <nikola.pujaz@gmail.com>
2020-10-07 14:36:46 +0200

    Add Loading Indicator

---
+++ client/src/app/core/services/busy.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BusyService {
+  busyRequestCount = 0;
+
+  constructor(private ngxSpinnerService: NgxSpinnerService) { }
+
+  // tslint:disable-next-line: typedef
+  busy() {
+    this.busyRequestCount++;
+    this.ngxSpinnerService.show(undefined, {
+      type: 'pacman',
+      bdColor: 'rgba(255,255,255,0.8)',
+      color: '#333333'
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  idle() {
+    this.busyRequestCount--;
+    if (this.busyRequestCount <= 0) {
+      this.busyRequestCount = 0;
+      this.ngxSpinnerService.hide();
+    }
+  }
+}
