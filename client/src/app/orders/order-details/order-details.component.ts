6050bcd63008b6abf1bd1b3365e5f33583feb23c
npdrums <nikola.pujaz@gmail.com>
2021-10-01 23:25:13 +0200

    Orders

---
+++ client/src/app/orders/order-details/order-details.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { IOrder } from 'src/app/shared/models/order';
+import { ActivatedRoute } from '@angular/router';
+import { OrdersService } from '../orders.service';
+
+@Component({
+  selector: 'app-order-detailed',
+  templateUrl: './order-details.component.html',
+  styleUrls: ['./order-details.component.scss']
+})
+export class OrderDetailsComponent implements OnInit {
+  order: IOrder;
+
+  constructor(private route: ActivatedRoute, private ordersService: OrdersService) { }
+
+  // tslint:disable-next-line: typedef
+  ngOnInit() {
+    this.ordersService.getOrderDetails(+this.route.snapshot.paramMap.get('id'))
+      .subscribe((order: IOrder) => {
+      this.order = order;
+    }, (error: any) => {
+      console.log(error);
+    });
+  }
+
+}
---
+++ client/src/app/orders/orders-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { OrdersComponent } from './orders.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
+
+const routes: Routes = [
+  {path: '', component: OrdersComponent},
+  {path: ':id', component: OrderDetailsComponent}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class OrdersRoutingModule { }
